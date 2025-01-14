76cd811b754061fff0bdc20c2989c24a0e132dc0
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-01-14 19:58:31 +0100

    made some changes

--- src/module/restAPIs/students/student.routes.ts
+++ src/module/restAPIs/students/student.routes.ts
@@ -6,14 +6,14 @@ const studentController = new StudentController();
 export const studentRoutes = Router();
 
 studentRoutes.post(
-  "/:id/books",
+  "/:studentId/books",
   async (req: Request, res: Response): Promise<void> => {
     await studentController.createBookForStudent(req, res);
   }
 );
 
 studentRoutes.get(
-  "/:id/books",
+  "/:studentId/books",
   async (req: Request, res: Response): Promise<void> => {
     await studentController.getBooksOfStudent(req, res);
   }
